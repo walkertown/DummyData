389c7c687d213d67d5f5c4e83c00d4e35b5bf16b
walkertown <48234962+walkertown@users.noreply.github.com>
2024-03-27 22:04:46 +0530

    Update gitClone.groovy

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -1,6 +1,7 @@
 def call(repo) {           
-  def branchName = 'main'
-  def urlName = 'https://github.com/walkertown/ShoppingApp.git'
-  def command = `git branch:'` + branchName + `' url:'` + urlName + `'`
+  "'Hello Triple" + "Multiple lines'";
+  def branchName = "'main'"
+  def urlName = "'https://github.com/walkertown/ShoppingApp.git'"
+  def command = "git branch:" + branchName + " url:" + urlName
   sh command
 }
